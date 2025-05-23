1bd950d2aeb2148931b9da4e3387b60db83cba21
Vinay Kanchanapally <vinay.kanchanapally@gmail.com>
2025-05-23 16:40:03 -0700

    test 1

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and SAVE to test hmr
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to study more
       </p>
     </>
   )
